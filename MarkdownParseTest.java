6a6fb29f9aff1d81a906096defd53de667741864
kyy006 <92092627+kyy006@users.noreply.github.com>
2022-01-27 10:45:47 -0800

    test2

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,10 @@
 import static org.junit.Assert.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.swing.text.AttributeSet.ParagraphAttribute;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +12,21 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    @Test
+    public void checkContent(){
+        //boolean contain=false;
+
+        //List.of("https://something.com","some-page.html")
+
+        try {
+            Path filePath=Path.of("test-file.md");
+            String content=Files.readString(filePath);
+            assertEquals("",content);
+            System.out.println(content);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            //contain=true;
+        }
+    }
+    
 }
